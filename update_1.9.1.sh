20b8ee9cd8f4fe2a3b16bd69ea1e2650ad5d94b7
cheyang <cheyang@163.com>
2015-08-09 01:22:06 +0800

    update irb dir

--- update_1.9.1.sh
+++ update_1.9.1.sh
@@ -1,4 +1,3 @@
-git pull
-gem build docker-metrics-ruby.gemspec
-gem install docker-metrics-0.0.1.gem
-irb
+/opt/chef/embedded/bin/gem build docker-metrics-ruby.gemspec
+/opt/chef/embedded/bin/gem install docker-metrics-0.0.1.gem
+/opt/chef/embedded/bin/irb
